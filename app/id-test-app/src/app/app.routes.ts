446893413a02f95469050b434a0bc5f87c6ee162
neox5 <faustmannchr@gmail.com>
2024-02-05 05:11:32 +0100

    Adds id provider logos

--- app/id-test-app/src/app/app.routes.ts
+++ app/id-test-app/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from "@angular/router";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    pathMatch: "full",
+    redirectTo: "login",
+  },
+  {
+    path: "login",
+    pathMatch: "full",
+    loadComponent: () =>
+      import("./login/login.component").then((m) => m.LoginComponent),
+  },
+];
--- app/id-test-app/src/app/login/login.component.ts
+++ app/id-test-app/src/app/login/login.component.ts
@@ -1,12 +1,91 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-login',
+  selector: "app-login",
   standalone: true,
   imports: [],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  template: `
+    <table>
+      <tr>
+        <th>Name</th>
+        <th>Action</th>
+      </tr>
+      @for (p of providers; track p.name) {
+      <tr>
+        <td>
+          <img [src]="p.logo" alt="{{ p.name }} logo" />
+        </td>
+        <td><button (click)="p.loginFn()">Login</button></td>
+      </tr>
+      }
+    </table>
+  `,
+  styleUrl: "./login.component.scss",
 })
 export class LoginComponent {
-
+  providers = [
+    {
+      name: "Auth0",
+      logo: "assets/logos/auth0.svg",
+      loginFn: () => {
+        console.log("Logging in with Auth0");
+      },
+    },
+    {
+      name: "Keycloak",
+      logo: "assets/logos/keycloak.svg",
+      loginFn: () => {
+        console.log("Logging in with Keycloak");
+      },
+    },
+    {
+      name: "Zitadel",
+      logo: "assets/logos/zitadel.svg",
+      loginFn: () => {
+        console.log("Logging in with Zitadel");
+      },
+    },
+    {
+      name: "Google",
+      logo: "assets/logos/google.svg",
+      loginFn: () => {
+        console.log("Logging in with Google");
+      },
+    },
+    {
+      name: "Firebase",
+      logo: "assets/logos/firebase.svg",
+      loginFn: () => {
+        console.log("Logging in with Firebase");
+      },
+    },
+    {
+      name: "Github",
+      logo: "assets/logos/github.svg",
+      loginFn: () => {
+        console.log("Logging in with Github");
+      },
+    },
+    {
+      name: "Apple",
+      logo: "assets/logos/apple.svg",
+      loginFn: () => {
+        console.log("Logging in with Apple");
+      },
+    },
+    {
+      name: "Microsoft",
+      logo: "assets/logos/microsoft.svg",
+      loginFn: () => {
+        console.log("Logging in with Microsoft");
+      },
+    },
+    {
+      name: "Meta",
+      logo: "assets/logos/meta.svg",
+      loginFn: () => {
+        console.log("Logging in with Meta");
+      },
+    },
+  ];
 }
