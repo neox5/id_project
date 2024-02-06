6828fbbde64b1166cd920082949f1a79dfdcbcb6
neox5 <faustmannchr@gmail.com>
2024-02-06 07:47:05 +0100

    Adds container scss

--- app/id-test-app/src/app/app.component.ts
+++ app/id-test-app/src/app/app.component.ts
@@ -5,7 +5,11 @@ import { RouterOutlet } from "@angular/router";
   selector: "app-root",
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: "./app.component.html",
+  template: `
+    <div class="container">
+      <router-outlet />
+    </div>
+  `,
   styleUrl: "./app.component.scss",
 })
 export class AppComponent {
